f36a72ca834441aaf72374538e0b261da95b7a38
prajwal kotalwar <prajwalkotalwar@gmail.com>
2020-10-17 12:32:18 +0530

    first commit

---
+++ Assets/health.cs
@@ -0,0 +1,20 @@
+﻿ using UnityEngine;
+ using System.Collections;
+ using UnityEngine.Events;
+ 
+     [System.Serializable]
+     public class HealthEvent : UnityEvent<float> { };
+ 
+  public class HealthUpdate : MonoBehaviour
+  {
+      public float health = 100.0f;
+      public HealthEvent healthEvent ;
+      private const float coef = 0.2f;
+      
+      void Update ()
+      {
+          health -= coef * Time.deltaTime;
+          if( healthEvent != null )
+              healthEvent.Invoke( health ) ;
+      }        
+  }
---
+++ Assets/move.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public Transform vcam;
+    public float toggleAngle=30.0f;
+    public float speed= 3.0f;
+    public bool moveForward;
+
+    private CharacterController cc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc=GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(vcam.eulerAngles.x <= toggleAngle && vcam.eulerAngles.x< 90.0f){
+            moveForward= true;
+        }
+        else{
+            moveForward=false;
+        }
+        if(moveForward){
+            Vector3 forward= vcam.TransformDirection(Vector3.forward);
+            cc.SimpleMove(forward*speed);
+        }
+    }
+    
+}
